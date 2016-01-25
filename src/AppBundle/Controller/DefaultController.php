6c0d9edfccd6fc8b08deb7cc7bfb3b4e867c1010
ghost0817 <sainzaya.b@gmail.com>
2016-01-26 01:28:34 +0800

    birthday list with calendar.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
 {
@@ -63,9 +64,35 @@ class DefaultController extends Controller
      */
     public function birthdaysAction(Request $request)
     {
+        $cal_days = array(
+            array('month' => 'January', 
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 1, date('Y')) ),
+            array('month' => 'February', 
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 2, date('Y')) ),
+            array('month' => 'March' ,
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 3, date('Y')) ),
+            array('month' => 'April' ,
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 4, date('Y')) ),
+            array('month' => 'May' ,
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 5, date('Y')) ),
+            array('month' => 'June' ,
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 6, date('Y')) ),
+            array('month' => 'July' ,
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 7, date('Y')) ),
+            array('month' => 'August' ,
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 8, date('Y')) ),
+            array('month' => 'September' ,
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 9, date('Y')) ),
+            array('month' => 'October' ,
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 10, date('Y')) ),
+            array('month' => 'November' ,
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 11, date('Y')) ),
+            array('month' => 'December' ,
+            'lday' => cal_days_in_month(CAL_GREGORIAN, 12, date('Y')) )
+            );
         // replace this example code with whatever you need
         return $this->render('default/birthdays.html.twig', array(
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+            'cal_days' => $cal_days,
         ));
     }
 }

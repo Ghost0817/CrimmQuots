45dfdeff050f0ea4049b0f79552e601744aec30f
Goodaway <sainzaya.b@gmail.com>
2016-01-26 08:23:05 +0800

    update brainyquots.sql
    update brainyquots.sql

--- brainyquots.sql
+++ brainyquots.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 20, 2016 at 11:27 AM
+-- Generation Time: Jan 26, 2016 at 01:22 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -28,29 +28,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `authors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nationality_id` int(11) NOT NULL,
-  `type_id` int(11) NOT NULL,
+  `nationality` int(11) NOT NULL,
   `born` date NOT NULL,
   `died` date NOT NULL,
   `description` varchar(255) NOT NULL,
   `body` text NOT NULL,
   `slug` varchar(255) NOT NULL,
-  `firstname` varchar(255) NOT NULL,
-  `lastname` varchar(255) NOT NULL,
+  `profession` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `nationality` (`nationality_id`,`type_id`),
-  KEY `type` (`type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `authors`
---
-
-INSERT INTO `authors` (`id`, `nationality_id`, `type_id`, `born`, `died`, `description`, `body`, `slug`, `firstname`, `lastname`) VALUES
-(1, 4, 1, '1902-02-20', '1984-04-22', '', '', 'ansel_adams', 'Adams', 'Ansel'),
-(2, 247, 2, '1952-03-11', '2001-05-11', '', '', 'douglas_adams', 'Adams', 'Douglas'),
-(3, 4, 4, '1838-02-16', '1918-03-27', '', '', 'henry_adams', 'Adams', 'Henry'),
-(4, 4, 3, '1735-10-30', '1826-07-04', '', '', 'john_adams', 'Adams', 'John');
+  KEY `nationality` (`nationality`),
+  KEY `profession` (`profession`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -61,262 +49,93 @@ INSERT INTO `authors` (`id`, `nationality_id`, `type_id`, `born`, `died`, `descr
 CREATE TABLE IF NOT EXISTS `nationality` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `country_code` varchar(2) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=248 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `nationality`
+-- Table structure for table `profession`
 --
 
-INSERT INTO `nationality` (`id`, `name`, `country_code`) VALUES
-(1, 'Afghanistan', 'AF'),
-(2, 'Albania', 'AL'),
-(3, 'Algeria', 'DZ'),
-(4, 'American Samoa', 'DS'),
-(5, 'Andorra', 'AD'),
-(6, 'Angola', 'AO'),
-(7, 'Anguilla', 'AI'),
-(8, 'Antarctica', 'AQ'),
-(9, 'Antigua and Barbuda', 'AG'),
-(10, 'Argentina', 'AR'),
-(11, 'Armenia', 'AM'),
-(12, 'Aruba', 'AW'),
-(13, 'Australia', 'AU'),
-(14, 'Austria', 'AT'),
-(15, 'Azerbaijan', 'AZ'),
-(16, 'Bahamas', 'BS'),
-(17, 'Bahrain', 'BH'),
-(18, 'Bangladesh', 'BD'),
-(19, 'Barbados', 'BB'),
-(20, 'Belarus', 'BY'),
-(21, 'Belgium', 'BE'),
-(22, 'Belize', 'BZ'),
-(23, 'Benin', 'BJ'),
-(24, 'Bermuda', 'BM'),
-(25, 'Bhutan', 'BT'),
-(26, 'Bolivia', 'BO'),
-(27, 'Bosnia and Herzegovina', 'BA'),
-(28, 'Botswana', 'BW'),
-(29, 'Bouvet Island', 'BV'),
-(30, 'Brazil', 'BR'),
-(31, 'British Indian Ocean Territory', 'IO'),
-(32, 'Brunei Darussalam', 'BN'),
-(33, 'Bulgaria', 'BG'),
-(34, 'Burkina Faso', 'BF'),
-(35, 'Burundi', 'BI'),
-(36, 'Cambodia', 'KH'),
-(37, 'Cameroon', 'CM'),
-(38, 'Canada', 'CA'),
-(39, 'Cape Verde', 'CV'),
-(40, 'Cayman Islands', 'KY'),
-(41, 'Central African Republic', 'CF'),
-(42, 'Chad', 'TD'),
-(43, 'Chile', 'CL'),
-(44, 'China', 'CN'),
-(45, 'Christmas Island', 'CX'),
-(46, 'Cocos (Keeling) Islands', 'CC'),
-(47, 'Colombia', 'CO'),
-(48, 'Comoros', 'KM'),
-(49, 'Congo', 'CG'),
-(50, 'Cook Islands', 'CK'),
-(51, 'Costa Rica', 'CR'),
-(52, 'Croatia (Hrvatska)', 'HR'),
-(53, 'Cuba', 'CU'),
-(54, 'Cyprus', 'CY'),
-(55, 'Czech Republic', 'CZ'),
-(56, 'Denmark', 'DK'),
-(57, 'Djibouti', 'DJ'),
-(58, 'Dominica', 'DM'),
-(59, 'Dominican Republic', 'DO'),
-(60, 'East Timor', 'TP'),
-(61, 'Ecuador', 'EC'),
-(62, 'Egypt', 'EG'),
-(63, 'El Salvador', 'SV'),
-(64, 'Equatorial Guinea', 'GQ'),
-(65, 'Eritrea', 'ER'),
-(66, 'Estonia', 'EE'),
-(67, 'Ethiopia', 'ET'),
-(68, 'Falkland Islands (Malvinas)', 'FK'),
-(69, 'Faroe Islands', 'FO'),
-(70, 'Fiji', 'FJ'),
-(71, 'Finland', 'FI'),
-(72, 'France', 'FR'),
-(73, 'France, Metropolitan', 'FX'),
-(74, 'French Guiana', 'GF'),
-(75, 'French Polynesia', 'PF'),
-(76, 'French Southern Territories', 'TF'),
-(77, 'Gabon', 'GA'),
-(78, 'Gambia', 'GM'),
-(79, 'Georgia', 'GE'),
-(80, 'Germany', 'DE'),
-(81, 'Ghana', 'GH'),
-(82, 'Gibraltar', 'GI'),
-(83, 'Guernsey', 'GK'),
-(84, 'Greece', 'GR'),
-(85, 'Greenland', 'GL'),
-(86, 'Grenada', 'GD'),
-(87, 'Guadeloupe', 'GP'),
-(88, 'Guam', 'GU'),
-(89, 'Guatemala', 'GT'),
-(90, 'Guinea', 'GN'),
-(91, 'Guinea-Bissau', 'GW'),
-(92, 'Guyana', 'GY'),
-(93, 'Haiti', 'HT'),
-(94, 'Heard and Mc Donald Islands', 'HM'),
-(95, 'Honduras', 'HN'),
-(96, 'Hong Kong', 'HK'),
-(97, 'Hungary', 'HU'),
-(98, 'Iceland', 'IS'),
-(99, 'India', 'IN'),
-(100, 'Isle of Man', 'IM'),
-(101, 'Indonesia', 'ID'),
-(102, 'Iran (Islamic Republic of)', 'IR'),
-(103, 'Iraq', 'IQ'),
-(104, 'Ireland', 'IE'),
-(105, 'Israel', 'IL'),
-(106, 'Italy', 'IT'),
-(107, 'Ivory Coast', 'CI'),
-(108, 'Jersey', 'JE'),
-(109, 'Jamaica', 'JM'),
-(110, 'Japan', 'JP'),
-(111, 'Jordan', 'JO'),
-(112, 'Kazakhstan', 'KZ'),
-(113, 'Kenya', 'KE'),
-(114, 'Kiribati', 'KI'),
-(115, 'Korea, Democratic People''s Republic of', 'KP'),
-(116, 'Korea, Republic of', 'KR'),
-(117, 'Kosovo', 'XK'),
-(118, 'Kuwait', 'KW'),
-(119, 'Kyrgyzstan', 'KG'),
-(120, 'Lao People''s Democratic Republic', 'LA'),
-(121, 'Latvia', 'LV'),
-(122, 'Lebanon', 'LB'),
-(123, 'Lesotho', 'LS'),
-(124, 'Liberia', 'LR'),
-(125, 'Libyan Arab Jamahiriya', 'LY'),
-(126, 'Liechtenstein', 'LI'),
-(127, 'Lithuania', 'LT'),
-(128, 'Luxembourg', 'LU'),
-(129, 'Macau', 'MO'),
-(130, 'Macedonia', 'MK'),
-(131, 'Madagascar', 'MG'),
-(132, 'Malawi', 'MW'),
-(133, 'Malaysia', 'MY'),
-(134, 'Maldives', 'MV'),
-(135, 'Mali', 'ML'),
-(136, 'Malta', 'MT'),
-(137, 'Marshall Islands', 'MH'),
-(138, 'Martinique', 'MQ'),
-(139, 'Mauritania', 'MR'),
-(140, 'Mauritius', 'MU'),
-(141, 'Mayotte', 'TY'),
-(142, 'Mexico', 'MX'),
-(143, 'Micronesia, Federated States of', 'FM'),
-(144, 'Moldova, Republic of', 'MD'),
-(145, 'Monaco', 'MC'),
-(146, 'Mongolia', 'MN'),
-(147, 'Montenegro', 'ME'),
-(148, 'Montserrat', 'MS'),
-(149, 'Morocco', 'MA'),
-(150, 'Mozambique', 'MZ'),
-(151, 'Myanmar', 'MM'),
-(152, 'Namibia', 'NA'),
-(153, 'Nauru', 'NR'),
-(154, 'Nepal', 'NP'),
-(155, 'Netherlands', 'NL'),
-(156, 'Netherlands Antilles', 'AN'),
-(157, 'New Caledonia', 'NC'),
-(158, 'New Zealand', 'NZ'),
-(159, 'Nicaragua', 'NI'),
-(160, 'Niger', 'NE'),
-(161, 'Nigeria', 'NG'),
-(162, 'Niue', 'NU'),
-(163, 'Norfolk Island', 'NF'),
-(164, 'Northern Mariana Islands', 'MP'),
-(165, 'Norway', 'NO'),
-(166, 'Oman', 'OM'),
-(167, 'Pakistan', 'PK'),
-(168, 'Palau', 'PW'),
-(169, 'Palestine', 'PS'),
-(170, 'Panama', 'PA'),
-(171, 'Papua New Guinea', 'PG'),
-(172, 'Paraguay', 'PY'),
-(173, 'Peru', 'PE'),
-(174, 'Philippines', 'PH'),
-(175, 'Pitcairn', 'PN'),
-(176, 'Poland', 'PL'),
-(177, 'Portugal', 'PT'),
-(178, 'Puerto Rico', 'PR'),
-(179, 'Qatar', 'QA'),
-(180, 'Reunion', 'RE'),
-(181, 'Romania', 'RO'),
-(182, 'Russian Federation', 'RU'),
-(183, 'Rwanda', 'RW'),
-(184, 'Saint Kitts and Nevis', 'KN'),
-(185, 'Saint Lucia', 'LC'),
-(186, 'Saint Vincent and the Grenadines', 'VC'),
-(187, 'Samoa', 'WS'),
-(188, 'San Marino', 'SM'),
-(189, 'Sao Tome and Principe', 'ST'),
-(190, 'Saudi Arabia', 'SA'),
-(191, 'Senegal', 'SN'),
-(192, 'Serbia', 'RS'),
-(193, 'Seychelles', 'SC'),
-(194, 'Sierra Leone', 'SL'),
-(195, 'Singapore', 'SG'),
-(196, 'Slovakia', 'SK'),
-(197, 'Slovenia', 'SI'),
-(198, 'Solomon Islands', 'SB'),
-(199, 'Somalia', 'SO'),
-(200, 'South Africa', 'ZA'),
-(201, 'South Georgia South Sandwich Islands', 'GS'),
-(202, 'Spain', 'ES'),
-(203, 'Sri Lanka', 'LK'),
-(204, 'St. Helena', 'SH'),
-(205, 'St. Pierre and Miquelon', 'PM'),
-(206, 'Sudan', 'SD'),
-(207, 'Suriname', 'SR'),
-(208, 'Svalbard and Jan Mayen Islands', 'SJ'),
-(209, 'Swaziland', 'SZ'),
-(210, 'Sweden', 'SE'),
-(211, 'Switzerland', 'CH'),
-(212, 'Syrian Arab Republic', 'SY'),
-(213, 'Taiwan', 'TW'),
-(214, 'Tajikistan', 'TJ'),
-(215, 'Tanzania, United Republic of', 'TZ'),
-(216, 'Thailand', 'TH'),
-(217, 'Togo', 'TG'),
-(218, 'Tokelau', 'TK'),
-(219, 'Tonga', 'TO'),
-(220, 'Trinidad and Tobago', 'TT'),
-(221, 'Tunisia', 'TN'),
-(222, 'Turkey', 'TR'),
-(223, 'Turkmenistan', 'TM'),
-(224, 'Turks and Caicos Islands', 'TC'),
-(225, 'Tuvalu', 'TV'),
-(226, 'Uganda', 'UG'),
-(227, 'Ukraine', 'UA'),
-(228, 'United Arab Emirates', 'AE'),
-(229, 'United Kingdom', 'GB'),
-(230, 'United States', 'US'),
-(231, 'United States minor outlying islands', 'UM'),
-(232, 'Uruguay', 'UY'),
-(233, 'Uzbekistan', 'UZ'),
-(234, 'Vanuatu', 'VU'),
-(235, 'Vatican City State', 'VA'),
-(236, 'Venezuela', 'VE'),
-(237, 'Vietnam', 'VN'),
-(238, 'Virgin Islands (British)', 'VG'),
-(239, 'Virgin Islands (U.S.)', 'VI'),
-(240, 'Wallis and Futuna Islands', 'WF'),
-(241, 'Western Sahara', 'EH'),
-(242, 'Yemen', 'YE'),
-(243, 'Yugoslavia', 'YU'),
-(244, 'Zaire', 'ZR'),
-(245, 'Zambia', 'ZM'),
-(246, 'Zimbabwe', 'ZW'),
-(247, 'English', 'EN');
+CREATE TABLE IF NOT EXISTS `profession` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=66 ;
+
+--
+-- Dumping data for table `profession`
+--
+
+INSERT INTO `profession` (`id`, `name`, `slug`) VALUES
+(1, 'Activist', 'activist'),
+(2, 'Actor', 'actor'),
+(3, 'Actress', 'actress'),
+(4, 'Architect', 'architect'),
+(5, 'Artist', 'artist'),
+(6, 'Astronaut', 'astronaut'),
+(7, 'Athlete', 'athlete'),
+(8, 'Author', 'author'),
+(9, 'Aviator', 'aviator'),
+(10, 'Businessman', 'businessman'),
+(11, 'Businesswoman', 'businesswoman'),
+(12, 'Cartoonist', 'cartoonist'),
+(13, 'Celebrity', 'celebrity'),
+(14, 'Chef', 'chef'),
+(15, 'Clergyman', 'clergyman'),
+(16, 'Coach', 'coach'),
+(17, 'Comedian', 'comedian'),
+(18, 'Composer', 'composer'),
+(19, 'Criminal', 'criminal'),
+(20, 'Critic', 'critic'),
+(21, 'Dancer', 'dancer'),
+(22, 'Designer', 'designer'),
+(23, 'Diplomat', 'diplomat'),
+(24, 'Director', 'director'),
+(25, 'Dramatist', 'dramatist'),
+(26, 'Driver', 'driver'),
+(27, 'Economist', 'economist'),
+(28, 'Editor', 'editor'),
+(29, 'Educator', 'educator'),
+(30, 'Entertainer', 'entertainer'),
+(31, 'Environmentalist', 'environmentalist'),
+(32, 'Explorer', 'explorer'),
+(33, 'First Lady', 'first_lady'),
+(34, 'Historian', 'historian'),
+(35, 'Inventor', 'inventor'),
+(36, 'Journalist', 'journalist'),
+(37, 'Judge', 'judge'),
+(38, 'Lawyer', 'lawyer'),
+(39, 'Leader', 'leader'),
+(40, 'Mathematician', 'mathematician'),
+(41, 'Model', 'model'),
+(42, 'Musician', 'musician'),
+(43, 'Novelist', 'novelist'),
+(44, 'Philosopher', 'philosopher'),
+(45, 'Photographer', 'photographer'),
+(46, 'Physicist', 'physicist'),
+(47, 'Playwright', 'playwright'),
+(48, 'Poet', 'poet'),
+(49, 'Politician', 'politician'),
+(50, 'President', 'president'),
+(51, 'Producer', 'producer'),
+(52, 'Psychologist', 'psychologist'),
+(53, 'Public Servant', 'public_servant'),
+(54, 'Publisher', 'publisher'),
+(55, 'Revolutionary', 'revolutionary'),
+(56, 'Royalty', 'royalty'),
+(57, 'Saint', 'saint'),
+(58, 'Scientist', 'scientist'),
+(59, 'Sculptor', 'sculptor'),
+(60, 'Sociologist', 'sociologist'),
+(61, 'Soldier', 'soldier'),
+(62, 'Statesman', 'statesman'),
+(63, 'Theologian', 'theologian'),
+(64, 'Vice President', 'vice_president'),
+(65, 'Writer', 'writer');
 
 -- --------------------------------------------------------
 
@@ -459,28 +278,6 @@ INSERT INTO `topics` (`id`, `name`, `slug`) VALUES
 (241, 'Women', 'women'),
 (242, 'Work', 'work');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `type`
---
-
-CREATE TABLE IF NOT EXISTS `type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `type`
---
-
-INSERT INTO `type` (`id`, `name`) VALUES
-(1, 'Photographer'),
-(2, 'Writer'),
-(3, 'President'),
-(4, 'Historian');
-
 --
 -- Constraints for dumped tables
 --
@@ -489,8 +286,8 @@ INSERT INTO `type` (`id`, `name`) VALUES
 -- Constraints for table `authors`
 --
 ALTER TABLE `authors`
-  ADD CONSTRAINT `authors_ibfk_1` FOREIGN KEY (`nationality_id`) REFERENCES `nationality` (`id`),
-  ADD CONSTRAINT `authors_ibfk_2` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`);
+  ADD CONSTRAINT `authors_ibfk_1` FOREIGN KEY (`nationality`) REFERENCES `nationality` (`id`),
+  ADD CONSTRAINT `authors_ibfk_3` FOREIGN KEY (`profession`) REFERENCES `profession` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
